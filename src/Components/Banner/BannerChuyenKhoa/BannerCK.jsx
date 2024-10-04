9aff1e62dfa9b5dd6bd563a2c8ff9a262828fa42
KimAnh2124 <kimanh0165359@gmail.com>
2024-10-04 18:22:49 +0700

    Thêm các banner Lienhe

---
+++ src/Components/Banner/BannerChuyenKhoa/BannerCK.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BannerCK.css';
+
+const BannerCK = () => {
+  return (
+    <div className='banner-ck'>
+        <img src='/Images/chuyenkhoa_banner.png' alt='BannerCK' className='banner-cuachuyenkhoa' />
+    </div>
+  );
+}
+
+export default BannerCK;
---
+++ src/Components/Banner/BannerHome/BannerTrangChu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Banner.css'
+
+const BannerTrangChu = () => {
+  return (
+    <div className='banner'>
+       <div className='banner-text'>
+        <p>Chăm Sóc <br/>Sức Khỏe <br/>Chất Lượng <br/>Quốc Tế</p>
+       </div>
+    </div>
+  )
+}
+
+export default BannerTrangChu
+
+
