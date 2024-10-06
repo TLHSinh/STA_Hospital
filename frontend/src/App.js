7bfbd745d35d4caf021003573b30884c10ee2bae
PhuTai18 <taidoan030204@gmail.com>
2024-10-06 22:35:31 +0700

    tao router va footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Outlet } from 'react-router-dom'; // Import Outlet để render các route con
+import Navbar from './Components/Navbar/Navbar'; // Import Navbar
+import Footer from './Components/Footer/Footer'; // Import Footer
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Navbar />
+      <Outlet /> {/* Đây là nơi các trang con sẽ được render */}
+      <Footer /> {/* Footer sẽ xuất hiện trên tất cả các trang */}
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,91 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './Pages/Home'; 
+
+
+
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <App />, 
+    children: [ 
+      {
+        path: "/", 
+        element: <Home />, 
+      },
+      {
+        path: "vechungtoi",
+        element: <VeChungToi />, 
+      },
+      {
+        path: "chuyenkhoa",
+        element: <ChuyenKhoa />,
+      },
+      {
+        path: "goikham",
+        element: <GoiKham />,
+      },
+      {
+        path: "bacsi",
+        element: <BacSi />,
+      },
+      {
+        path: "lienhe",
+        element: <LienHe />,
+      },
+      {
+        path: "datlichhen",
+        element: <DatLichHen />,
+      },
+      {
+        path: "bacsi/bs-tran-kim-duong", // Thêm route cho Card1
+        element: <Card1 />,
+      },
+      {
+        path: "bacsi/bs-le-thi-anh", // Thêm route cho Card1
+        element: <Card2 />,
+      },
+      {
+        path: "bacsi/bs-chu-minh-tuan", // Thêm route cho Card1
+        element: <Card3 />,
+      },
+      {
+        path: "bacsi/bs-bui-thi-truc-my", // Thêm route cho Card1
+        element: <Card4 />,
+      },
+      {
+        path: "bacsi/bs-nguyen-man-nhi", // Thêm route cho Card1
+        element: <Card5 />,
+      },
+      {
+        path: "bacsi/bs-nguyen-mai-huy", // Thêm route cho Card1
+        element: <Card6 />,
+      },
+      
+      {
+        path: "/login", 
+        element: <Login />,
+      },
+      {
+        path: "/register", 
+        element: <Register />,
+      },
+      // Bạn có thể thêm các route khác ở đây
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
