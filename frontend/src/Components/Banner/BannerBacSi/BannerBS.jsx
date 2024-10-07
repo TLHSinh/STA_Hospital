6106b1d4f063d60dd3e83e2ccd29d892fc5bcad8
KimAnh2124 <kimanh0165359@gmail.com>
2024-10-07 15:02:27 +0700

    them banner bac si

---
+++ frontend/src/Components/Banner/BannerBacSi/BannerBS.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BannerBS.css';
+
+const BannerBS = () => {
+  return (
+    <div className='banner-bs'>
+        <img src='/Images/bacsi_banner.png' alt='BannerBS' className='banner-cuabacsi' />
+    </div>
+  );
+}
+
+export default BannerBS;
