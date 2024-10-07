87f85147e427105820ff99c0be6ae558e9a2bc16
KimAnh2124 <kimanh0165359@gmail.com>
2024-10-07 14:30:27 +0700

    them card bac si

---
+++ frontend/src/Pages/DetailBacSi/card2.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import BannerBS from '../../Components/Banner/BannerBacSi/BannerBS';
+
+function BS1() {
+    return (
+        <div>
+          <BannerBS/>
+          <div className='container-content'>
+            <div className='intro'>
+              <h1>THÔNG TIN BÁC SĨ</h1>
+            </div>
+    
+            
+          </div>
+        </div>
+      )
+}
+
+export default BS1;
