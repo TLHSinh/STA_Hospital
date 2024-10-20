dacfd0269e2af800829449afe3434e337c6690bb
TLHSinh <trinhlehongsinh04@gmail.com>
2024-10-20 17:09:54 +0700

    Phân quyền người dùng bác sĩ admin

--- backend/routes/doctor.js
+++ backend/routes/doctor.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import { getAllDoctor, getSingleDoctor, updateDoctor, deleteDoctor } from '../controllers/doctorController.js';
 
+import { authenticate , restrict} from '../auth/veryfyToken.js';
+
 const router = express.Router();
+
 
 // Đảm bảo route đăng ký sử dụng phương thức POST
 router.get('/:id', getSingleDoctor);
-router.get('/', getAllDoctor);
-router.put('/:id', updateDoctor);
-router.delete('/:id', deleteDoctor);
+router.get('/',getAllDoctor);
+router.put('/:id', authenticate, restrict(["doctor"]), updateDoctor);
+router.delete('/:id', authenticate, restrict(["doctor"]), deleteDoctor);
 
 export default router;
